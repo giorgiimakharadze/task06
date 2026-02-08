a544ab8479fddbf1b7db271ed4607b48493bed0a
Giorgi Makharadze <giorgimakharadze4@gmail.com>
2026-02-08 21:11:45 +0400

    edited version

--- task06/versions.tf
+++ task06/versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.110.0, < 4.0.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.0, < 4.0.0"
+    }
   }
 }
 
