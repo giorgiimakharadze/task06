9aac2dc81408a40131e4300bbad052d08ac8e45a
Giorgi Makharadze <giorgimakharadze4@gmail.com>
2026-02-08 21:55:31 +0400

    added connection string

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -38,3 +38,17 @@ output "sql_admin_password" {
   description = "Admin Password"
   sensitive   = true
 }
+
+
+output "sql_connection_string" {
+  description = "ADO.NET connection string for Azure SQL Database using SQL authentication."
+  sensitive   = true
+  value = format(
+    "Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+    azurerm_mssql_server.this.fully_qualified_domain_name,
+    azurerm_mssql_database.this.name,
+    var.admin_login,
+    random_password.sql_admin_password.result
+  )
+}
+
